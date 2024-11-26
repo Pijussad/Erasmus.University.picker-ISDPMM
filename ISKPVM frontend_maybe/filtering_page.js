696eaf60263c23ace2f5eacace9b4e39b4c2655b
Aleksas-7 <aleksas.grinys@gmail.com>
2024-11-26 18:36:17 +0200

    Db connection with maaaaaaaany issues to be fixes, someday, maybe

--- ISKPVM frontend_maybe/filtering_page.js
+++ ISKPVM frontend_maybe/filtering_page.js
@@ -1,30 +1,146 @@
 
 
-// Obtaining information from filter page options
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { getFirestore, collection, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCDrG2mniJa9zEJMm6n4MGf6NLdWH4cbzM",
+  authDomain: "erasmus-gidas.firebaseapp.com",
+  projectId: "erasmus-gidas",
+  storageBucket: "erasmus-gidas.appspot.com",
+  messagingSenderId: "687630827561",
+  appId: "1:687630827561:web:fc2414b36f4226abfd5c38",
+  measurementId: "G-NW8RK7LQR9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db  = getFirestore(app);
+
+
+async function queryUniversities(filters) {
+    try {
+        let q = collection(db, "universities");
+
+        let queries = [];
+        for (const [field, value] of Object.entries(filters)) {
+            if (value !== "-") {
+                if (field === "kalba"){
+                    queries.push(where("kalbos", "array-contains", value));
+                }
+                else {
+                    queries.push(where(field, "==", value));
+                }
+                
+            }
+        }
+
+        // Combine queries using Firestore's query() function
+        if (queries.length > 0) {
+            q = query(q, ...queries);
+        }
+
+        console.log("Querys", q);
+
+        // Fetch data from Firestore
+        const querySnapshot = await getDocs(q);
+        const results = [];
+        querySnapshot.forEach((doc) => {
+            results.push({ id: doc.id, ...doc.data() });
+        });
+    
+        return results;
+    } catch (error) {
+        console.error("Error querying universities:", error);
+        return [];
+    }
+}
+
+
+function uniObjToString(univ){
+    return Object.entries(univ)
+        .map(([field, value]) => `${field}=${value}`)
+        .join("\n");
+}
+
+function format_and_output(res){
+
+// Object { 
+// id: "zn2LoYnGf5cs2FWCltGS", 
+// bakalauroT: "9", 
+// doktoranturosT: "", 
+// sritis: "061 Informacinės ir ryšių technologijos",
+// magistroT: "9", 
+// universitetas: "Bahcesehir University", 
+// kalbos: (1) […], 
+// url: "http://www.bahcesehir.edu.tr/", 
+// kodas: "TR ISTANBU08", 
+// fakultetas: "Matematikos ir informatikos fakultetas"}
+
+    let a = [];
+    res.forEach((univ) => {
+        a.push("<pre class=\"form-panel-test\">" + uniObjToString(univ) + "</pre>")
+    });
+
+    console.log(a);
+
+    let out = $("#resOutput");
+    out.html(a);
+}
+
+// Obtaining information from filter page options
 $(document).ready(function () {
 
 
 
-
-    $('#filters-form').on('submit', function (event) {
+    // Forma baigta, turime rodiklius pagal kuriuos reikia rasti universitetus
+    $('#filters-form').on('submit', async function (event) {
         event.preventDefault(); 
 
-        const fakultetas = $('#faculty').val();
-        const programa = $('#program').val();
-        const studijuTipas = $('#studyType').val();
-        const semestras = $('#semester').val();
-        const salis = $('#country').val();
-        const miestas = $('#city').val();
-        const regionas = $('#region').val();
-        const universitetas = $('#university').val();
-        const kalba = $('#language').val();
-        const qsIvertinimas = $('#qsRating').val();
-        const pragyvenimoIslaidos = $('#livingCosts').val();
+        const _fakultetas = $('#faculty').val();
+        const _programa = $('#program').val();
+        const _studijuTipas = $('#studyType').val();
+        const _semestras = $('#semester').val();
+        const _salis = $('#country').val();
+        const _miestas = $('#city').val();
+        const _regionas = $('#region').val();
+        const _universitetas = $('#university').val();
+        const _kalba = $('#language').val();
+        const _qsIvertinimas = $('#qsRating').val();
+        const _pragyvenimoIslaidos = $('#livingCosts').val();
 
-        console.log(`Fakultetas: ${fakultetas}\nStudijų programa: ${programa}\nStudijų tipas: ${studijuTipas}\nSemestras: ${semestras}\nŠalis: ${salis}\nMiestas: ${miestas}\nRegionas: ${regionas}\nUniversitetas: ${universitetas}\nStudijų kalba: ${kalba}\nQS įvertinimas: ${qsIvertinimas}\nPragyvenimo išlaidos: ${pragyvenimoIslaidos}\n\n`);
+        //console.log(`Fakultetas: ${fakultetas}\nStudijų programa: ${programa}\nStudijų tipas: ${studijuTipas}\nSemestras: ${semestras}\nŠalis: ${salis}\nMiestas: ${miestas}\nRegionas: ${regionas}\nUniversitetas: ${universitetas}\nStudijų kalba: ${kalba}\nQS įvertinimas: ${qsIvertinimas}\nPragyvenimo išlaidos: ${pragyvenimoIslaidos}\n\n`);
 
+        const filters = {
+            fakultetas: _fakultetas,
+            programa: _programa,
+            studijuTipas: _studijuTipas,
+            semestras: _semestras,
+            salis: _salis,
+            miestas: _miestas,
+            regionas: _regionas,
+            universitetas: _universitetas,
+            kalba: _kalba
+            // qsIvertinimas: _qsIvertinimas,
+            // pragyvenimoIslaidos: _pragyvenimoIslaidos
+        };
+
+        console.log(filters);
+
+        const res = await queryUniversities(filters);
+
+        if (res.length != 0){
+            format_and_output(res);
+        }
+        else {
+            console.log("Failed getting query results !!!");
+        }
 
         // Connection to DB to get information
     });
 });
+
+
+
+
+
