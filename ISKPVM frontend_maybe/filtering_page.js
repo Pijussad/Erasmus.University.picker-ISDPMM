2f99f90039d63be6b59acc388994b4544fd13409
adomas ragelis <adomasragelis@gmail.com>
2024-12-17 20:25:24 +0200

    new_design

--- ISKPVM frontend_maybe/filtering_page.js
+++ ISKPVM frontend_maybe/filtering_page.js
@@ -1,7 +1,5 @@
-
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-import { getFirestore, collection, getDocs, query, where, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
+import { getFirestore, collection, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCDrG2mniJa9zEJMm6n4MGf6NLdWH4cbzM",
@@ -13,51 +11,38 @@ const firebaseConfig = {
   measurementId: "G-NW8RK7LQR9"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db  = getFirestore(app);
-
 
 async function queryUniversities(filters) {
     try {
         let q = collection(db, "universities");
-
         let queries = [];
         for (const [field, value] of Object.entries(filters)) {
             if (value !== "-") {
                 if (field === "kalba"){
                     queries.push(where("kalbos", "array-contains", value));
-                }
-                else if (field === "fakultetas"){
+                } else if (field === "fakultetas"){
                     queries.push(where(field, "==", value));
-                }
-                else {
+                } else {
                     queries.push(where(field, "==", value));
                 }
             }
         }
-
-        // Combine queries using Firestore's query() function
         if (queries.length > 0) {
             q = query(q, ...queries);
         }
-
-        console.log("Querys", q);
-
-        // Fetch data from Firestore
         const querySnapshot = await getDocs(q);
         const results = [];
         querySnapshot.forEach((doc) => {
             results.push({ id: doc.id, ...doc.data() });
         });
-    
         return results;
     } catch (error) {
         console.error("Error querying universities:", error);
         return [];
     }
 }
-
 
 function uniObjToString(univ){
     return Object.entries(univ)
@@ -66,38 +51,69 @@ function uniObjToString(univ){
 }
 
 function format_and_output(res){
-
-// Object { 
-// id: "zn2LoYnGf5cs2FWCltGS", 
-// bakalauroT: "9", 
-// doktoranturosT: "", 
-// sritis: "061 Informacinės ir ryšių technologijos",
-// magistroT: "9", 
-// universitetas: "Bahcesehir University", 
-// kalbos: (1) […], 
-// url: "http://www.bahcesehir.edu.tr/", 
-// kodas: "TR ISTANBU08", 
-// fakultetas: "Matematikos ir informatikos fakultetas"}
-
-    let a = [];
+    const out = $("#resOutput");
+    out.empty();
+    $(".results-count").text(`Rezultatų: ${res.length}`);
     res.forEach((univ) => {
-        a.push("<pre class=\"form-panel-test\">" + uniObjToString(univ) + "</pre>")
+        const card = $(`
+            <div class="university-card">
+                <div class="university-card-header">
+                    <img src="https://via.placeholder.com/60" alt="Univ Image">
+                    <div>
+                        <h4 style="margin:0;">${univ.universitetas}</h4>
+                        <small>${univ.salis || ''}${univ.miestas ? ', ' + univ.miestas : ''}</small>
+                    </div>
+                </div>
+                <div class="university-card-content">
+                    <div class="university-details">
+                        <div class="university-details-column">
+                            <h5>Fakultetas</h5>
+                            <p>${univ.fakultetas || '-'}</p>
+                            <h5>Studijų sritys</h5>
+                            <p>${univ.sritis || '-'}</p>
+                            <h5>Kalbos</h5>
+                            <p>${(univ.kalbos || []).join(', ') || '-'}</p>
+                        </div>
+                        <div class="university-details-column">
+                            <h5>Studijų lygis</h5>
+                            <p>${univ.studijuTipas || '-'}</p>
+                            <h5>URL</h5>
+                            <a href="${univ.url || '#'}" target="_blank">${univ.url || 'N/A'}</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `);
+        card.on('click', function() {
+            $(this).toggleClass('expanded');
+        });
+        out.append(card);
     });
-
-    //console.log(a);
-
-    let out = $("#resOutput");
-    out.html(a);
 }
 
-// Obtaining information from filter page options
 $(document).ready(async function () {
+    const emptyfilters = {
+        fakultetas: "-",
+        programa: "-",
+        studijuTipas: "-",
+        semestras: "-",
+        salis: "-",
+        miestas: "-",
+        regionas: "-",
+        universitetas: "-",
+        kalba: "-"
+    };
+    const res = await queryUniversities(emptyfilters);
+    if (res.length != 0){
+        format_and_output(res);
+        find_unique_and_populate_html(res);
+    } else {
+        console.log("Failed getting query results !!!");
+    }
 
     $('button.btn-primary').on('click',async function() {
-        console.log("llo");
-
         const _fakultetas = $('#faculty').val() || "-";
-        const _programa = $('#program').val() || "-" || "-";
+        const _programa = $('#program').val() || "-";
         const _studijuTipas = $('#studyType').val() || "-";
         const _semestras = $('#semester').val() || "-";
         const _salis = $('#country').val() || "-";
@@ -105,10 +121,6 @@ $(document).ready(async function () {
         const _regionas = $('#region').val() || "-";
         const _universitetas = $('#university').val() || "-";
         const _kalba = $('#language').val() || "-";
-        const _qsIvertinimas = $('#qsRating').val() || "-";
-        const _pragyvenimoIslaidos = $('#livingCosts').val() || "-";
-
-        //console.log(`Fakultetas: ${fakultetas}\nStudijų programa: ${programa}\nStudijų tipas: ${studijuTipas}\nSemestras: ${semestras}\nŠalis: ${salis}\nMiestas: ${miestas}\nRegionas: ${regionas}\nUniversitetas: ${universitetas}\nStudijų kalba: ${kalba}\nQS įvertinimas: ${qsIvertinimas}\nPragyvenimo išlaidos: ${pragyvenimoIslaidos}\n\n`);
 
         const filters = {
             fakultetas: _fakultetas,
@@ -120,89 +132,47 @@ $(document).ready(async function () {
             regionas: _regionas,
             universitetas: _universitetas,
             kalba: _kalba
-            // qsIvertinimas: _qsIvertinimas,
-            // pragyvenimoIslaidos: _pragyvenimoIslaidos
         };
-
-        console.log(filters);
-        console.log("Jello");
-
         const res = await queryUniversities(filters);
-
-        if (res.length != 0){
-            format_and_output(res);
-        }
-        else {
-            console.log("Failed getting query results !!!");
-        }
-
-        // Connection to DB to get information
+        format_and_output(res);
     });
-    
-
-
-
 });
 
-
-
+async function fetchFaculties() {
+    const querySnapshot = await getDocs(collection(db, 'VU_courses'));
+    const faculties = [];
+    if (!querySnapshot.empty) {
+        querySnapshot.forEach(doc => {
+            faculties.push(doc.id);
+        });
+    }
+    return faculties;
+}
 
 function find_unique_and_populate_html(res) {
-
-    //Language
     const _language = $('#language');
-    const uniqueLanguage = [...new Set(res.flatMap(res => res.kalbos))];
+    const uniqueLanguage = [...new Set(res.flatMap(r => r.kalbos || []))];
     uniqueLanguage.forEach((el) => {
         _language.append(`<option>${el}</option>`)
     });
-
-    //Countrys
     const _countrys = $('#country');
-    const uniqueCountrys = [...new Set(res.map(res => res.salis))];
+    const uniqueCountrys = [...new Set(res.map(r => r.salis).filter(Boolean))];
     uniqueCountrys.forEach((el) => {
         _countrys.append(`<option>${el}</option>`)
     });
-
-    // University
     const _universitys = $('#university');
-    const uniqueUniversitys = [...new Set(res.map(res => res.universitetas))];
+    const uniqueUniversitys = [...new Set(res.map(r => r.universitetas).filter(Boolean))];
     uniqueUniversitys.forEach((el) => {
         _universitys.append(`<option>${el}</option>`)
     });
-
+    const _cities = $('#city');
+    const uniqueCities = [...new Set(res.map(r => r.miestas).filter(Boolean))];
+    uniqueCities.forEach((el) => {
+        _cities.append(`<option>${el}</option>`)
+    });
+    const _regions = $('#region');
+    const uniqueRegions = [...new Set(res.map(r => r.regionas).filter(Boolean))];
+    uniqueRegions.forEach((el) => {
+        _regions.append(`<option>${el}</option>`)
+    });
 }
-
-const emptyfilters = {
-    fakultetas: "-",
-    programa: "-",
-    studijuTipas: "-",
-    semestras: "-",
-    salis: "-",
-    miestas: "-",
-    regionas: "-",
-    universitetas: "-",
-    kalba: "-"
-    // qsIvertinimas: _qsIvertinimas,
-    // pragyvenimoIslaidos: _pragyvenimoIslaidos
-};
-
-
-let all_the_info = []
-const res = await queryUniversities(emptyfilters);
-
-if (res.length != 0){
-    format_and_output(res);
-}
-else {
-    console.log("Failed getting query results !!!");
-}
-
-find_unique_and_populate_html(res)
-
-
-
-
-
-
-
-
